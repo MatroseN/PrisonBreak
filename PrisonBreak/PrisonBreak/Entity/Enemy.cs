51521fd5d7c2e15de6a61f007b0122f9dc76b264
felix <felix.lundh@gmail.com>
2020-10-09 13:17:06 +0200

    update

---
+++ PrisonBreak/PrisonBreak/Entity/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PrisonBreak.Entity {
+    public class Enemy : Entity, Character {
+        public Enemy(Game game, Vector2 position) : base(game, position) {
+
+        }
+
+        public override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+        }
+
+        private void move() {
+
+        }
+    }
+}
